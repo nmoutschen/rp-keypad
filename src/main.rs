0d42bc92c04dcc98553ba3513e2bfcb57be6204e
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2021-12-18 20:03:43 +0100

    feat: add bongo ferris

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@
 #![no_main]
 
 use cortex_m_rt::entry;
+use embedded_graphics::{
+    image::{Image, ImageRawLE},
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
 use embedded_hal::prelude::*;
 use embedded_time::{fixed_point::*, rate::*};
 use hal::pac::interrupt;
 use heapless::Vec;
 use panic_halt as _;
 use rp_keypad::hal::{self, pac, prelude::*};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 use usb_device::{class_prelude::*, prelude::*};
 use usbd_midi::data::byte::u7::U7;
 use usbd_midi::data::midi::channel::Channel;
@@ -21,6 +29,9 @@ use usbd_midi::{data::usb::constants::USB_CLASS_NONE, midi_device::MidiClass};
 #[no_mangle]
 #[used]
 pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+static FERRIS1: &[u8] = include_bytes!("../ferris1.raw");
+static FERRIS2: &[u8] = include_bytes!("../ferris2.raw");
 
 static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
 static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
@@ -63,17 +74,33 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Setup keypad
-    let keypad_sda = pins.keypad_sda.into_mode::<hal::gpio::FunctionI2C>();
-    let keypad_scl = pins.keypad_scl.into_mode::<hal::gpio::FunctionI2C>();
-    let mut keypad_i2c = hal::I2C::i2c0(
+    // Setup I2C bus
+    let pin_sda = pins.sda.into_mode::<hal::gpio::FunctionI2C>();
+    let pin_scl = pins.scl.into_mode::<hal::gpio::FunctionI2C>();
+    let i2c = hal::I2C::i2c0(
         pac.I2C0,
-        keypad_sda,
-        keypad_scl,
+        pin_sda,
+        pin_scl,
         400.kHz(),
         &mut pac.RESETS,
         clocks.peripheral_clock.freq(),
     );
+    let i2c_bus = shared_bus::BusManagerSimple::new(i2c);
+
+    // Setup display
+    let interface = I2CDisplayInterface::new(i2c_bus.acquire_i2c());
+    let mut display = Ssd1306::new(interface, DisplaySize128x32, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    // Load ferris
+    let ferris1: ImageRawLE<BinaryColor> = ImageRawLE::new(FERRIS1, 128);
+    let ferris1_img = Image::new(&ferris1, Point::zero());
+
+    let ferris2: ImageRawLE<BinaryColor> = ImageRawLE::new(FERRIS2, 128);
+    let ferris2_img = Image::new(&ferris2, Point::zero());
+
+    let ferris_imgs = [ferris1_img, ferris2_img];
 
     // Setup leds
     let _leds_sclk = pins.leds_sclk.into_mode::<hal::gpio::FunctionSpi>();
@@ -116,6 +143,11 @@ fn main() -> ! {
         pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
     };
 
+    // Draw ferris
+    let mut ferris_cur = 0;
+    ferris_imgs[ferris_cur].draw(&mut display).unwrap();
+    display.flush().unwrap();
+
     // Setup data
     // 16 bits for 16 keys
     let mut prev_keys = 0u16;
@@ -128,8 +160,8 @@ fn main() -> ! {
 
     loop {
         // Read keyboard state
-        keypad_i2c.write(0x20, &[0]).unwrap();
-        keypad_i2c.read(0x20, &mut key_data).unwrap();
+        i2c_bus.acquire_i2c().write(0x20, &[0]).unwrap();
+        i2c_bus.acquire_i2c().read(0x20, &mut key_data).unwrap();
         let keys = u16::from_le_bytes(key_data) ^ 0xffff;
 
         // Light up leds for pressed keys
@@ -146,6 +178,18 @@ fn main() -> ! {
         }
         // Send midi data
         if let Some(message) = messages.pop() {
+            // Invert ferris
+            match message {
+                Message::NoteOn { .. } => {
+                    ferris_cur ^= 1;
+                    display.clear();
+                    ferris_imgs[ferris_cur].draw(&mut display).unwrap();
+                    display.flush().unwrap();
+                }
+                _ => {}
+            }
+
+            // Send MIDI message
             let midi = unsafe { MIDI.as_mut().unwrap() };
             midi.send_message(UsbMidiEventPacket {
                 cable_number: CableNumber::Cable0,
@@ -172,6 +216,7 @@ fn set_leds(data: &mut [u8; 72], leds: u16, brightness: u8, r: u8, g: u8, b: u8)
     for i in 0..16 {
         // Brightness
         data[i * 4 + 4] = 0b11100000 | brightness;
+        // Color
         if leds & (1 << i) != 0 {
             data[i * 4 + OFFSET + 1] = b;
             data[i * 4 + OFFSET + 2] = g;
@@ -220,22 +265,22 @@ impl Into<Option<Message>> for KeySwitch {
 
 fn into_note(pos: u8) -> Note {
     match pos {
-        0 => Note::C3,
-        1 => Note::D3,
-        2 => Note::E3,
-        3 => Note::F3,
-        4 => Note::G3,
+        0 => Note::E3,
+        1 => Note::F3,
+        2 => Note::Fs3,
+        3 => Note::G3,
+        4 => Note::Gs3,
         5 => Note::A3,
-        6 => Note::B3,
-        7 => Note::C4,
-        8 => Note::D4,
-        9 => Note::E4,
-        10 => Note::F4,
-        11 => Note::G4,
-        12 => Note::A4,
-        13 => Note::B4,
-        14 => Note::C5,
-        15 => Note::D5,
+        6 => Note::As3,
+        7 => Note::B3,
+        8 => Note::C4,
+        9 => Note::Cs4,
+        10 => Note::D4,
+        11 => Note::Ds4,
+        12 => Note::E4,
+        13 => Note::F4,
+        14 => Note::Fs4,
+        15 => Note::G4,
         _ => panic!("Invalid note position"),
     }
 }
